483b3653b33397d112fd2fbbe7b48eadaa7665fc
J A <jarata@gmail.com>
2019-01-21 14:06:33 -0800

    building SearchBar

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <div>
+            <div>
+                <h1>Instagram</h1>
+            </div>
+            <div>
+                <input type="text" placeholder="Search" />
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar
