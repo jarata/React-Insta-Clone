d30ecfda1ad0da882065d2c96b2b63114184173c
J A <jarata@gmail.com>
2019-01-21 14:09:27 -0800

    SearchBar built and imported font-awesome library

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,13 +4,16 @@ const SearchBar = () => {
     return (
         <div>
             <div>
+                <i className="fab fa-instagram"></i>
                 <h1>Instagram</h1>
             </div>
             <div>
                 <input type="text" placeholder="Search" />
             </div>
             <div>
-
+                <i className="far fa-compass"></i>
+                <i className="far fa-heart"></i>
+                <i className="far fa-user"></i>
             </div>
         </div>
     )
