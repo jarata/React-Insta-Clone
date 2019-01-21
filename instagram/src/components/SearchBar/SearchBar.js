f12b2ac492a67205ed9d896264af1659d6a38dd8
J A <jarata@gmail.com>
2019-01-21 15:35:00 -0800

    styling SearchBar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import SearchBarStyles from './SearchBarStyles.css'
 
 const SearchBar = () => {
     return (
-        <div>
-            <div>
+        <div className="searchbar-container">
+            <div className="logo">
                 <i className="fab fa-instagram"></i>
                 <h1>Instagram</h1>
             </div>
             <div>
                 <input type="text" placeholder="Search" />
             </div>
-            <div>
+            <div className="settings-container">
                 <i className="far fa-compass"></i>
                 <i className="far fa-heart"></i>
                 <i className="far fa-user"></i>
