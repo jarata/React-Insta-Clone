f58d84820120b981d9b089fa152f9162f33fdcbd
J A <jarata@gmail.com>
2019-01-21 14:13:21 -0800

    Comment built

---
+++ instagram/src/components/CommentSection/Comment.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Comment = props => {
+    console.log(props)
+    return (
+        <div>
+            {
+                props.comments.map(comment => {
+                    console.log(comment);
+                    return (
+                        <div>
+                            <p><comment.username</p>
+                            <p>{comment.text}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Comment
